3674b39283e45cf14c55a78732ee409285b6d64c
Siger Ma <siger.ma@mail.mcgill.ca>
2022-02-26 20:46:08 -0500

    Added resume button and made buttons interactive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import profile_pic from "./Images/EFL_0515.jpg"
-import Icons from "./Components/Icons/Icons.js";
+import ReactTooltip from 'react-tooltip';
+import profile_pic from "./Images/EFL_0515.jpg";
+import {Icon1, Icon2, Icon3, Icon4} from "./Components/Icons/Icons.js";
 import './App.css';
 
 
@@ -9,7 +10,30 @@ function App() {
       <header className="title">Siger Ma</header>
       <img className = "profile_pic" src={profile_pic} alt=""></img>
       <p className="description">I am a hardworking first year student in Software Engineering Co-op.</p>
-      <Icons></Icons>
+      <button className = "button1" data-tip data-for="LinkedIn">
+        <Icon1></Icon1>
+      </button>
+      <ReactTooltip id='LinkedIn' effect='solid' backgroundColor= '#3b83f7'>
+        LinkedIn
+      </ReactTooltip>
+      <button className = "button2" data-tip data-for="GitHub">
+        <Icon2></Icon2>
+      </button>
+      <ReactTooltip id='GitHub' effect='solid' backgroundColor= '#1e0342'>
+        GitHub
+      </ReactTooltip>
+      <button className = "button3" data-tip data-for="Email">
+        <Icon3></Icon3>
+      </button>
+      <ReactTooltip id='Email' effect='solid' backgroundColor='#373738'>
+        Email
+      </ReactTooltip>
+      <button className = "button4" data-tip data-for="Resume">
+        <Icon4></Icon4>
+      </button>
+      <ReactTooltip id='Resume' effect='solid' backgroundColor='#fa362f'>
+        Resume
+      </ReactTooltip>
     </div>
   );
 }
--- src/Components/Icons/Icons.js
+++ src/Components/Icons/Icons.js
@@ -1,21 +1,40 @@
 import React from 'react'
-import linkedin from "../../Images/img_24873.png"
+import linkedin from "../../Images/linkedin.png"
 import github from "../../Images/github.png"
 import mail from "../../Images/mail.png"
+import cv from "../../Images/cv.png"
+import resume from "../../Images/Siger MA-Resume.pdf"
 
-function Icons() {
+function Icon1() {
     return (
-        <><a className="icon-link1" href="https://www.linkedin.com/in/siger-ma/">
+        <a className="icon-link1" href="https://www.linkedin.com/in/siger-ma/">
             <img className="linkedin" src={linkedin} alt=""></img>
         </a>
-        <a className="icon-link2" href="https://github.com/sigerma88">
-                <img className="github" src={github} alt=""></img>
-        </a>
-        <a className="icon-link3" href={'mailto:siger.ma@mail.mcgill.ca'}>
-                <img className="mail" src={mail} alt=""></img>
-        </a>
-        </>
     )
 }
 
-export default Icons
+function Icon2() {
+    return (
+        <a className="icon-link2" href="https://github.com/sigerma88">
+                <img className="github" src={github} alt=""></img>
+        </a>
+    )
+}
+
+function Icon3() {
+    return (
+        <a className="icon-link3" href={'mailto:siger.ma@mail.mcgill.ca'}>
+                <img className="mail" src={mail} alt=""></img>
+        </a>
+    )
+}
+
+function Icon4() {
+    return (
+        <a className="icon-link4" href={resume}>
+                <img className="cv" src={cv} alt=""></img>
+        </a>
+    )
+}
+
+export {Icon1, Icon2, Icon3, Icon4}
