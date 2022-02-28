2b57986b56bd74da15f5bb622bce6ab59b699ccf
Siger Ma <siger.ma@mail.mcgill.ca>
2022-02-27 20:05:47 -0500

    Fixed bookshelf button and added comments about limitations

--- src/Components/Bookshelf/Bookshelf.js
+++ src/Components/Bookshelf/Bookshelf.js
@@ -9,17 +9,21 @@ function Bookshelf() {
                 <TabList className="tabs">
                     <Tab>Read</Tab>
                     <Tab>Reading</Tab>
-                    <Tab>Recommandations</Tab>
+                    <Tab>Recommendations</Tab>
                 </TabList>
 
                 <TabPanel>
                     <Table1 />
+                    <p>This list is incomplete :(</p>
+                    <p>Many more books will be added in the future!</p>
                 </TabPanel>
                 <TabPanel>
                     <Table2 />
                 </TabPanel>
                 <TabPanel>
                     <Table3 />
+                    <p>Currently, recommendations are not recorded :(</p>
+                    <p>So, if you really want to make a recommendation, don't hesitate to send me an email!</p>
                 </TabPanel>
             </Tabs>
         </div>
